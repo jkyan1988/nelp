0b2e0fcb6485352801b2dcd520b86a0671d2605f
James Kyan <james.kyan88@gmail.com>
2022-08-01 11:44:37 -0400

    no errors?

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,13 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module ReactRailsApiProjectTemplate
   class Application < Rails::Application
@@ -39,7 +40,7 @@ module ReactRailsApiProjectTemplate
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
